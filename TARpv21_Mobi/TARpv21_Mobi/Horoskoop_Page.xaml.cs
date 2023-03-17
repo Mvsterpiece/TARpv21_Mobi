ac2caa0f0196809a33b3f0d15b358a88791b954b
opilane <opilane@HP-CZC2349HTQ>
2023-03-17 09:41:31 +0200

    Add project files.

---
+++ TARpv21_Mobi/TARpv21_Mobi/Horoskoop_Page.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Internals;
+using Xamarin.Forms.Xaml;
+
+namespace TARpv21_Mobi
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Horoskoop_Page : ContentPage
+    {
+
+        public Horoskoop_Page()
+        {
+            DatePicker data = new DatePicker();
+            data.DateSelected += Data_DateSelected;
+            Image img = new Image
+            {
+                Source = "horoskoop.jpg",
+                WidthRequest= 150,
+                HeightRequest= 150,
+            };
+
+
+
+            StackLayout st = new StackLayout { Orientation = StackOrientation.Vertical, BackgroundColor = Color.Gray, Children= { img, data } };
+            Content = st;
+
+
+
+        }
+
+        private void Data_DateSelected(object sender, DateChangedEventArgs e)
+        {
+            
+        }
+    }
+}
---
+++ TARpv21_Mobi/TARpv21_Mobi/Start_Page.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace TARpv21_Mobi
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Start_Page : ContentPage
+    {
+        List<ContentPage> contentPages = new List<ContentPage>() { new Horoskoop_Page() };
+        List<string> tekstid = new List<string> { "Horoskoop" };
+        public Start_Page()
+        {
+
+            StackLayout st = new StackLayout
+            {
+                Orientation = StackOrientation.Vertical,
+                BackgroundColor = Color.Gray
+            };
+            for (int i = 0; i < contentPages.Count; i++)
+            {
+                Button button = new Button
+                {
+                    Text = tekstid[i],
+                    TabIndex = i,
+                    BackgroundColor = Color.AliceBlue,
+                    TextColor = Color.Black
+                };
+                button.Clicked += Navig_funktsion;
+                st.Children.Add(button);
+            }
+            Content = st;
+        }
+        private async void Navig_funktsion(object sender, EventArgs e)
+        {
+            Button b = (Button)sender;
+            await Navigation.PushAsync(contentPages[b.TabIndex]);
+        }
+    }
+
+}
